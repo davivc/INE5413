549368cb892a165389616c5886f2fbf530c2f007
Davi Candido (Note) <davivc@gmail.com>
2023-08-17 19:34:42 -0300

    Dfs

--- 01_grafo.cpp
+++ 01_grafo.cpp
@@ -7,15 +7,21 @@ class Graph {
   public:
     Graph(int VV, int EE) : V(VV) , E(EE) {
         adjs = vector<vector<int> > (V);
+        dfsMarked = vector<bool > (V, false);
+        dfsEdgeTo = vector<int > (V);
     }
 
     void add_edge(int u, int v) {
         adjs[u].push_back(v);
         adjs[v].push_back(u);
     }
+    
     int get_V() { return V; }                          // Informa nro de vértices deste grafo
-    int get_E() { return E; }                          // Informa nro de arestas deste grafo
+    
+    int get_E() { return E; }    
+                          // Informa nro de arestas deste grafo
     vector<int> get_adjs(int u) { return adjs[u]; }    // Informa os vértices adjacentes a u dado
+    
     void imprime() {
         for(int u = 0; u < V; u++) {
             cout << u << ": ";
@@ -25,9 +31,35 @@ class Graph {
         }
     }
 
+    bool check_dfs(int v) {
+        if (dfsMarked[v] == true) { return true; }
+        else { return false; }
+    }
+
+    void dfs(int v) {
+        dfsMarked[v] = true;
+        vector<int> v_adjs = get_adjs(v);
+
+        for(int i = 0; i < v_adjs.size(); i++) {
+            if(check_dfs(v_adjs[i]) == false) {
+                dfsEdgeTo[v_adjs[i]] = v;
+                dfs(v_adjs[i]);
+            }
+        }
+    }
+
+    void print_dfs() {
+        for(int u = 0; u < V; u++) {
+            cout << u << ": " << dfsMarked[u] <<  " - " << dfsEdgeTo[u];
+            cout << '\n';
+        }
+    }
+
   private:
     int V, E;                       // Nro de vértices e de arestas
     vector<vector<int>> adjs;       // Lista de adjacências
+    vector<bool> dfsMarked;
+    vector<int> dfsEdgeTo;
 };
 
 int main() {
@@ -42,6 +74,8 @@ int main() {
     }
 
     G->imprime();
+    G->dfs(3);
+    G->print_dfs();
 
     return 0;
 }
